52bbf1009eed9c6f08a0d5f68555463595371b6d
michelebruno <43304601+michelebruno@users.noreply.github.com>
2021-09-30 15:46:00 +0200

    For loop works

--- star-clustering.py
+++ star-clustering.py
@@ -6,41 +6,70 @@ from sklearn.cluster import AgglomerativeClustering
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-import kaleido
 
 # pd.options.plotting.backend = "plotly"
 
-df = pd.read_csv(r'database_NorthernEmisphere.csv', low_memory=False, usecols=["ra", "dec"])
+df = pd.read_csv(r'database_NorthernEmisphere.csv', low_memory=False)
 
-print(df.shape)
-fig = go.Figure(
+# Selected columns
+df_sl = df.iloc[:, 7:9]
+# Inizializza la figura
+fig = go.Figure()
 
-)
+ac = AgglomerativeClustering()
 
-fig.add_trace(go.Scatterpolar(
-    r=df['dec'],
-    theta=[datum['ra'] * 360 / 24 for index, datum in df.iterrows()],
-    mode='markers',
-    name='Figure 8',
+ac.fit(df_sl.values)
 
-    marker=dict(
-        color="red",
-        # symbol="square",
-        size=8
-    )
-))
+# Associa a ciascun index dei dati, il cluster di appartenenza
+clustered_data = pd.DataFrame([df_sl.index, ac.labels_]).T
+
+# fig.add_trace(go.Scatterpolar(
+#     r=df['dec'],
+#     theta=[datum['ra'] * 360 / 24 for index, datum in df.iterrows()],
+#     mode='markers',
+#     name='Figure 8',
+#     marker=dict(
+#         color="red",
+#         # symbol="square",
+#         size=8
+#     )
+# ))
+
+
+grouped_indexes = clustered_data.groupby(1)
+
+for label in range(grouped_indexes.ndim):
+    indexes = grouped_indexes.groups[label]
+
+    filtered = df.iloc[indexes]
+
+    # print(filtered['dec'])
+    fig.add_trace(go.Scatterpolar(
+        r=filtered['dec'],
+        theta=[datum['ra'] * 360 / 24 for index, datum in filtered.iterrows()],
+        mode='markers',
+        name='Figure 8',
+        marker=dict(
+            color="red" if label > 0 else 'blue',
+            # symbol="square",
+            size=8
+        )
+    ))
+
+#
+
 
 fig.update_layout(polar=dict(
     radialaxis=dict(range=[90, 0]),
     # angularaxis=dict(showticklabels=False, ticks='')
 ))
 
-# fig.show()
+fig.show()
 
 if not os.path.exists("images"):
     os.mkdir("images")
 
-fig.write_image("images/fig1.svg",  engine="kaleido")
+# fig.write_image("images/fig1.svg", engine="kaleido")
 
 #
 # data = df.values
